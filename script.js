0130a4a07ee3a674b878f61835641cacd5754104
itsanindyak <iankoley04@gmail.com>
2024-02-13 15:49:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper = document.querySelector('.wrapper');
+const question = document.querySelector('.question');
+const yesBtn = document.querySelector('.yes-btn');
+const noBtn = document.querySelector('.no-btn');
+const wrapperRect = wrapper.getBoundingClientRect();
+const noBtnRect = noBtn.getBoundingClientRect();
+const questionMark = document.querySelector('.questionMark')
+const loveMark = document.querySelector('.loveMark')
+
+loveMark.style.display ='none';
+yesBtn.addEventListener('click', () => {
+
+    var a = new Audio('ddlj.mp3');
+    a.play()
+    question.innerHTML = 'I Love You :)';
+    question.style.position = "relative"; 
+    question.style.top = "50px";   
+    noBtn.style.display ='none';
+    yesBtn.style.display ='none';
+    loveMark.style.display ='block';
+    questionMark.style.display ='none';
+    
+});
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height - noBtnRect.height)) + 1;
+    noBtn.style.left = i + 'px';
+    noBtn.style.top = j + 'px';
+});
